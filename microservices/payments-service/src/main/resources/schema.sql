4e3d150288578b574500ea98f984c7c4a5bae6e6
dmartinezsepulveda.external@epo.org <dmartinezsepulveda.external@epo.org>
2024-10-08 16:55:08 +0200

    adding initial code

--- microservices/payments-service/src/main/resources/schema.sql
+++
@@ -1,7 +0,0 @@
--- Crear tabla PAGO
-CREATE TABLE PAYMENTS (
-                      id BIGSERIAL PRIMARY KEY,
-                      order_id BIGINT,
-                      amount DOUBLE PRECISION NOT NULL,
-                      discount VARCHAR(255)
-);
--- microservices/products-service/src/main/java/com/example/products_service/client/InventoryClient.java
+++
@@ -1,6 +0,0 @@
-package com.example.products_service.client;
-
-public interface InventoryClient {
-
-    void addStock(Long productId, int quantity);
-}
